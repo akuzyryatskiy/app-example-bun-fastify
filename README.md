c38137ee9d2457d7f6c94c89f49e715c3d1092cc
Кузырятский Александр <a.kuzyryatskiy@timeweb.ru>
2026-05-21 10:13:58 +0200

    example repo

---
+++ README.md
@@ -0,0 +1,2 @@
+﻿# app-example-bun-fastify
+╨Я╤А╨╕╨╝╨╡╤А ╨┐╤А╨╕╨╗╨╛╨╢╨╡╨╜╨╕╤П Fastify ╨╜╨░ Bun ╨┤╨╗╤П ╤В╨╡╤Б╤В╨╛╨▓ Timeweb Cloud App Platform.
---
+++ app.js
@@ -0,0 +1,15 @@
+﻿'use strict';
+const fastify = require('fastify')({ logger: true });
+const port = Number(process.env.PORT || 3000);
+fastify.get('/', async () => {
+  return { message: 'Timeweb Cloud + Fastify on Bun = тЭдя╕П' };
+});
+const start = async () => {
+  try {
+    await fastify.listen({ port, host: '0.0.0.0' });
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
+};
+start();
